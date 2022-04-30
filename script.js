3d14994b25e9f38eee5be44f54f2e81c23db3c23
BrainPath <victoria.queen11@yandex.ru>
2022-04-30 21:59:10 +0300

    finish

---
+++ script.js
@@ -0,0 +1,12 @@
+var text = document.querySelector ('text');
+
+text.onclick = function () {
+    text.style.color = 'red'
+}
+
+
+// let buttonprev = document.querySelector ('#gallery .buttons .prev');
+
+// buttonprev.onclick = function () {
+// alert(1);
+// }
